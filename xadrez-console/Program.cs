d1f288d880885d9e892eb555c2b7fdb507393e43
lemeson <lemeson93@gmail.com>
2023-04-04 15:44:44 -0300

    criada classe posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tabuleiro;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Posicao P;
+
+        P = new Posicao(3, 4);
+
+        Console.WriteLine("Posicao: " + P);
+
+        Console.ReadLine();
+    }
+}
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿
+namespace tabuleiro
+{
+    internal class Posicao{
+        //encapsulamento
+        public int linha { get; set;  }
+        public int coluna { get; set; }
+
+        //construtor
+        public Posicao(int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            return linha
+                + " , "
+                + coluna;
+        }
+    }
+}
