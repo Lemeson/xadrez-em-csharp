465df189d34f5dc1ef001be31c10829e91ad962b
lemeson <lemeson93@gmail.com>
2023-04-04 16:29:29 -0300

    implementando o tabuleiro

--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace tabuleiro
 {
-    internal class Posicao{
+     class Posicao{
         //encapsulamento
         public int linha { get; set;  }
         public int coluna { get; set; }
---
+++ xadrez-console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,16 @@
+﻿namespace tabuleiro
+{
+    class Tabuleiro
+    {
+        public int linha { get; set; }
+        public int colunas { get; set; }
+
+        private Peca[,] pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            this.linha = linhas;
+        }
+
+    }
+}
