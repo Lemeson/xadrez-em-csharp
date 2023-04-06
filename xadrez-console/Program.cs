1c953e9fa0fc8cfba763fa6fdf744c9bbac9ec63
lemeson <lemeson93@gmail.com>
2023-04-06 13:22:01 -0300

    retrição de posições

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,20 +8,11 @@ namespace xadrez_console
     {
         private static void Main(string[] args)
         {
-            try
-            {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 0));
+            Console.WriteLine(pos);
 
-                Tela.imprimirTabuleiro(tab);
-            }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos.toPosicao());
 
             Console.ReadLine();
         }
